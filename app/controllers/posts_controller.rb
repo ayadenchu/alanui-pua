019de7da06baddbc82f1cee7a457a6e0389cecd5
ayadenchu <saion.denchu316@gmail.com>
2020-11-18 16:39:06 +0900

    編集機能実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 before_action :authenticate_user!
+before_action :set_post, only: [:show, :edit, :update,:destroy]
 
   def index
     @posts = Post.all.order("created_at DESC")
@@ -19,11 +20,37 @@ before_action :authenticate_user!
   end
 
   def show
-    @post = Post.find(params[:id])
+  end
+
+  def edit
+    unless current_user == @post.user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(post_params)
+    else
+        render :edit
+    end
+  end
+
+  def destroy
+    if current_user == @post.user
+      @post.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
   
   private
   def post_params
     params.require(:post).permit(:title, :text, :category_id,:image).merge(user_id: current_user.id)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
