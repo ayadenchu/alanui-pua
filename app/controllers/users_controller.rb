200134dee43b889ff7cfcc585ad4a125ce3859d0
ayadenchu <saion.denchu316@gmail.com>
2020-11-25 13:03:55 +0900

    view調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
     @posts = current_user.posts.order("created_at DESC")
+
+    likes = Like.where(user_id: current_user.id).pluck(:post_id)  # ログイン中のユーザーのお気に入りのpost_idカラムを取得
+    @like_list = Post.find(likes) 
   end
   
 end
