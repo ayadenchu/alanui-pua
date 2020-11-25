3d624965e85f48d816e48a30f88be4ce27629872
ayadenchu <saion.denchu316@gmail.com>
2020-11-25 17:44:50 +0900

    未ログインユーザーの表示制限

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
-before_action :authenticate_user!, expect: [:index, :show]
-before_action :set_post, only: [:show, :edit, :update,:destroy]
+before_action :authenticate_user!,except: [:index,:show]
+before_action :set_post,only: [:show, :edit, :update,:destroy]
 
   def index
     @posts = Post.all.order("created_at DESC")
   end
+
 
   def new
     @post = Post.new
